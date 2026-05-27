10f7363d03f2c88ca647021f8ccc0a23221f713d
Hans-J. Schmid <knock@myopendoor.de>
2026-05-27 22:25:18 +0200

    The Stack, the Heap, and Pointers

--- easy-rust-010/src/main.rs
+++ easy-rust-010/src/main.rs
@@ -1,3 +1,13 @@
+// The Stack, the Heap, and Pointers
+// https://dhghomon.github.io/easy_rust/Chapter_12.html
+
 fn main() {
-  println!("Hello, world!");
+  let my_number = 42;
+  let single_reference = &my_number;
+  let double_reference = &single_reference;
+  let five_references = &&&&&my_number;
+  println!("{}", double_reference);
+  println!("{}", five_references);
+  println!("{}", my_number == **double_reference);
+  println!("{}", double_reference == ***five_references);
 }
