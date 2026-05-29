2d21b4701dd8ee65e7b2b5ecd529368dc62d81d3
Hans-J. Schmid <knock@myopendoor.de>
2026-05-29 05:59:05 +0200

    References and the dot operator
    no video

--- easy-rust-037/src/main.rs
+++ easy-rust-037/src/main.rs
@@ -2,6 +2,9 @@
 // https://dhghomon.github.io/easy_rust/Chapter_28.html
 // https://www.youtube.com/watch?v=vJSb7-YcrHc&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
 
+// References and the dot operator
+// https://dhghomon.github.io/easy_rust/Chapter_29.html
+
 #[derive(Debug)]
 struct Person {
   name: String,
@@ -10,7 +13,26 @@ struct Person {
   happiness: bool,
 }
 
+struct Item {
+  number: u8,
+}
+
+impl Item {
+  fn compare_number(&self, other_number: u8) {
+    println!(
+      "Are {} and {} equal? {}",
+      self.number, // we don't need to write *self.number
+      other_number,
+      self.number == other_number
+    );
+  }
+}
+
 fn main() {
+  // ///////////// //
+  // Destructuring //
+  // ///////////// //
+
   {
     let papa_doc = Person {
       name: "Papa Doc".to_string(),
@@ -81,5 +103,57 @@ fn main() {
     } = papa_doc;
 
     println!("They call him {} but his real name is {}.", a, b);
+  }
+
+  println!();
+
+  // /////////////////////////////// //
+  // References and the dot operator //
+  // /////////////////////////////// //
+
+  // {
+  //   let my_number = 9;
+  //   let reference = &my_number;
+  //
+  //   println!("{}", my_number == reference); // ⚠️ can't compare `{integer}` with `&{integer}`
+  // }
+
+  {
+    let my_number = 9;
+    let reference = &my_number;
+
+    println!("{}", my_number == *reference);
+  }
+
+  // {
+  //   let item = Item { number: 8 };
+  //
+  //   let reference_number = &item.number;
+  //
+  //   println!("{}", reference_number == 8); // ⚠️ can't compare `&u8` with `{integer}`
+  // }
+
+  {
+    // with the dot operator, we don't need * (does the dereferencing for you)
+
+    let item = Item { number: 8 };
+
+    let reference_item = &item;
+
+    println!("{}", reference_item.number == 8); // we don't need to write *reference_item.number
+  }
+
+  {
+    // when you use the dot operator (.), you don't need to worry about dereferencing (*)
+
+    let item = Item { number: 8 };
+
+    let ref_item = &item;
+    let ref_ref_item = &ref_item;
+
+    // works in any case
+    item.compare_number(8);
+    ref_item.compare_number(8);
+    ref_ref_item.compare_number(8);
   }
 }
