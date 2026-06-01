5570a56a9575e1f858b16e9ffb103cce793d52bc
Hans-J. Schmid <knock@myopendoor.de>
2026-06-01 22:05:43 +0200

    The todo! macro

--- easy-rust-114/src/main.rs
+++ easy-rust-114/src/main.rs
@@ -1,3 +1,52 @@
+// The todo! macro
+// https://dhghomon.github.io/easy_rust/Chapter_44.html
+// https://www.youtube.com/watch?v=In0js0GOdvM&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+
+// Another similar macro is `unimplemented!`.
+//
+// The difference between `unimplemented!` and `todo!` is that while `todo!`
+// conveys an intent of implementing the functionality later and the message is "not yet
+// implemented", `unimplemented!` makes no such claims. Its message is "not implemented".
+//
+// Also, some IDEs will mark `todo!`s.
+
+#![allow(unused)]
+
+struct Book {}
+
+enum BookType {
+  HardCover,
+  SoftCover,
+}
+
+fn get_book(book: &Book) -> Option<String> {
+  todo!()
+}
+
+fn delete_book(book: Book) -> Result<(), String> {
+  unimplemented!(); // ⚠️ panics at runtime!
+  // todo!() // ⚠️ panics at runtime!
+}
+
+// ⚠️ needs real type for compilation
+// fn get_book(book: &Book) -> WorldsBestType {
+//   todo!()
+// }
+
+// TODO: impl block and make these functions methods...
+
+// let's make sure the match statement works …
+fn check_book_type(book_type: &BookType) {
+  match book_type {
+    BookType::HardCover => println!("It's hardcover."),
+    BookType::SoftCover => println!("It's softcover."),
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  let book_type = BookType::HardCover;
+  check_book_type(&book_type);
+
+  let my_book = Book {};
+  delete_book(my_book);
 }
--- easy-rust-115/src/main.rs
+++ easy-rust-115/src/main.rs
@@ -1,3 +1,95 @@
+// Rc
+// https://dhghomon.github.io/easy_rust/Chapter_45.html
+// https://www.youtube.com/watch?v=T9UXIn-nUkw&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+// https://www.youtube.com/watch?v=kdh08-JTG78&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+// https://www.youtube.com/watch?v=JUPyGrGPzaY&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+// https://www.youtube.com/watch?v=RhlBvuZvwy8&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+
+#![allow(unused)]
+
+// A single-threaded reference-counting pointer.
+// 'Rc' stands for 'Reference Counted'.
+
+use std::rc::Rc;
+
+fn takes_a_string(input: String) {
+  println!("It is: {}", input)
+}
+
+fn also_takes_a_string(input: String) {
+  println!("It is: {}", input)
+}
+
+#[derive(Debug)]
+struct City1 {
+  name: String,
+  population: u32,
+  city_history: String,
+}
+
+#[derive(Debug)]
+struct CityData1 {
+  names: Vec<String>,
+  histories: Vec<String>,
+}
+
+#[derive(Debug)]
+struct City2 {
+  name: String,
+  population: u32,
+  city_history: Rc<String>, // String inside an Rc
+}
+
+#[derive(Debug)]
+struct CityData2 {
+  names: Vec<String>,
+  histories: Vec<Rc<String>>, // Vec of Strings inside Rcs
+}
+
 fn main() {
-  println!("Hello, world!");
+  // {
+  //   let user_name = String::from("User MacUserson");
+  //
+  //   takes_a_string(user_name);
+  //   also_takes_a_string(user_name); // ⚠️ use of moved value: `user_name`
+  // }
+
+  // {
+  //   let calgary = City1 {
+  //     name: "Calgary".to_string(),
+  //     population: 1_200_000,
+  //     city_history: "Calgary began as a fort called Fort Calgary that …".to_string(),
+  //   };
+  //
+  //   let canada_cities = CityData1 {
+  //     names: vec![calgary.name], // this is using calgary.name, which is short
+  //     histories: vec![calgary.city_history], // but this String is very long; ⚠️ value moved here [E0382]
+  //   };
+  //
+  //   println!("Calgary's history is: {}", calgary.city_history); // ⚠️ borrow of moved value: `calgary.city_history`
+  // }
+
+  {
+    let calgary = City2 {
+      name: "Calgary".to_string(),
+      population: 1_200_000,
+      // Pretend that this string is very very long
+      city_history: Rc::new("Calgary began as a fort called Fort Calgary that...".to_string()),
+    };
+
+    // A clone of an Rc just clones a pointer - it's basically free!
+
+    let canada_cities = CityData2 {
+      names: vec![calgary.name],                     // this is actually a move
+      histories: vec![calgary.city_history.clone()], // .clone() to increase the count
+    };
+
+    println!("Calgary's history is: {}", calgary.city_history);
+
+    println!("{}", Rc::strong_count(&calgary.city_history)); // 2
+    let new_owner = calgary.city_history.clone();
+    println!("{}", Rc::strong_count(&calgary.city_history)); // 3
+    drop(canada_cities);
+    println!("{}", Rc::strong_count(&calgary.city_history)); // 2
+  }
 }
