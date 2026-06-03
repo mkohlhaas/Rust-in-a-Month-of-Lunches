356f3f6f53c90fb1da711c6e6baf18b221c8c554
Hans-J. Schmid <knock@myopendoor.de>
2026-06-03 20:04:28 +0200

    Deref and DerefMut

--- easy-rust-138/src/main.rs
+++ easy-rust-138/src/main.rs
@@ -1,3 +1,156 @@
+// Default and the builder pattern
+// https://dhghomon.github.io/easy_rust/Chapter_55.html
+// https://www.youtube.com/watch?v=BA4eCOv--3M&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk&index=138
+// https://www.youtube.com/watch?v=mY1kVT5JajI&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk&index=139
+// https://www.youtube.com/watch?v=JsXp3zI4c_M&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk&index=140
+
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum LifeState {
+  Alive,
+  Dead,
+  NeverAlive,
+  Uncertain,
+}
+
+#[derive(Debug)]
+struct Character {
+  name: String,
+  age: u8,
+  height: u32,
+  weight: u32,
+  lifestate: LifeState,
+  can_use: bool,
+}
+
+impl Character {
+  fn new() -> Self {
+    Self {
+      name: "Billy".to_string(),
+      age: 15,
+      height: 170,
+      weight: 70,
+      lifestate: LifeState::Alive,
+      can_use: true,
+    }
+  }
+
+  // for builder pattern; consumes self and always returns Self
+  fn height(mut self, height: u32) -> Self {
+    self.height = height;
+    self.can_use = false;
+    self
+  }
+
+  fn weight(mut self, weight: u32) -> Self {
+    self.weight = weight;
+    self.can_use = false;
+    self
+  }
+
+  fn name(mut self, name: &str) -> Self {
+    self.name = name.to_string();
+    self.can_use = false;
+    self
+  }
+
+  fn build(mut self) -> Result<Character, String> {
+    if self.height < 200 && self.weight < 300 && !self.name.to_lowercase().contains("smurf") {
+      self.can_use = true;
+      Ok(self)
+    } else {
+      Err("Could not create character!".to_string())
+    }
+  }
+}
+
+impl Default for Character {
+  fn default() -> Self {
+    Self {
+      name: "Billy".to_string(),
+      age: 15,
+      height: 170,
+      weight: 70,
+      lifestate: LifeState::Alive,
+      can_use: true,
+    }
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  {
+    let default_i8_1: i8 = Default::default();
+    let default_i8_2 = i8::default();
+    let default_str_1: String = Default::default();
+    let default_str_2 = String::default();
+    let default_bool_1: bool = Default::default();
+    let default_bool_2 = bool::default();
+
+    println!(
+      "{:#?}",
+      (
+        default_i8_1,   // 0
+        default_i8_2,   // 0
+        default_str_1,  // ""
+        default_str_2,  // ""
+        default_bool_1, // false
+        default_bool_2, // false
+      )
+    );
+  }
+
+  println!();
+
+  {
+    let character = Character::default();
+
+    println!("{:?}", character);
+  }
+
+  println!();
+
+  // builder pattern
+  {
+    let mut character1: Character = Default::default();
+    character1 = character1.height(180).weight(60).name("Bobby");
+
+    let character2 = Character::default().height(180).weight(60).name("Bobby");
+
+    println!("{:?}", character1);
+    println!("{:?}", character2);
+  }
+
+  println!();
+
+  {
+    let character = Character::new().height(180).weight(60).name("Bobby");
+
+    println!("{:?}", character);
+  }
+
+  println!();
+
+  {
+    // will be declined
+    let character_with_smurf = Character::new().name("Lol I am Smurf!!").build();
+    let character_too_tall = Character::new().height(400).build();
+    let character_too_heavy = Character::new().weight(500).build();
+
+    // ✓
+    let character_ok = Character::new()
+      .name("Billybrobby")
+      .height(180)
+      .weight(100)
+      .build();
+
+    let character_vec = vec![
+      character_with_smurf,
+      character_too_tall,
+      character_too_heavy,
+      character_ok,
+    ];
+
+    println!("{:#?}", character_vec);
+  }
 }
--- easy-rust-141/src/main.rs
+++ easy-rust-141/src/main.rs
@@ -1,3 +1,184 @@
+// Deref and DerefMut
+// https://dhghomon.github.io/easy_rust/Chapter_56.html
+// https://www.youtube.com/watch?v=wXAbnDCACGY&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+// https://www.youtube.com/watch?v=6WR7AiyU1cg&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+// https://www.youtube.com/watch?v=C-CViDtRcaw&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+// https://www.youtube.com/watch?v=O7Ta07s3pNM&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk
+
+#![allow(dead_code)]
+
+use std::ops::{Deref, DerefMut};
+
+#[derive(Debug)]
+struct HoldsANumber(u8);
+
+impl HoldsANumber {
+  fn prints_the_number_times_two(&self) {
+    println!("{}", self.0 * 2);
+  }
+}
+
+impl Deref for HoldsANumber {
+  type Target = u8;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl DerefMut for HoldsANumber {
+  // You don't need type Target = u8; here because it already knows thanks to Deref
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    // Everything else is the same except it says mut everywhere
+    &mut self.0
+  }
+}
+
+struct DerefExample<T> {
+  value: T,
+}
+
+impl<T> Deref for DerefExample<T> {
+  type Target = T;
+
+  fn deref(&self) -> &Self::Target {
+    &self.value
+  }
+}
+
+enum Alignment {
+  Good,
+  Neutral,
+  Evil,
+}
+
+struct Character {
+  name: String,
+  strength: u8,
+  dexterity: u8,
+  health: u8,
+  intelligence: u8,
+  wisdom: u8,
+  charm: u8,
+  hit_points: i8,
+  alignment: Alignment,
+}
+
+impl Character {
+  fn new(
+    name: String,
+    strength: u8,
+    dexterity: u8,
+    health: u8,
+    intelligence: u8,
+    wisdom: u8,
+    charm: u8,
+    hit_points: i8,
+    alignment: Alignment,
+  ) -> Self {
+    Self {
+      name,
+      strength,
+      dexterity,
+      health,
+      intelligence,
+      wisdom,
+      charm,
+      hit_points,
+      alignment,
+    }
+  }
+}
+
+// impl Deref for Character. Now we can do any integer math we want! ⚠️ WEIRD!!!
+impl Deref for Character {
+  type Target = i8;
+
+  fn deref(&self) -> &Self::Target {
+    &self.hit_points
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  // ⚠️ can't compare `i32` with `&i32`
+  // {
+  //   let value = 7; // This is an i32
+  //   let reference = &7; // This is a &i32
+  //   println!("{}", value == reference);
+  // }
+
+  {
+    let value = 7;
+    let reference = &7;
+    println!("{}", value == *reference);
+  }
+
+  // ⚠️ type `HoldsANumber` cannot be dereferenced
+  // {
+  //   let my_number = HoldsANumber(20);
+  //   println!("{}", *my_number + 20);
+  // }
+
+  {
+    let x = DerefExample { value: 'a' };
+    assert_eq!('a', *x);
+  }
+
+  // Deref
+  {
+    let my_number = HoldsANumber(20);
+    println!("{:?}", *my_number + 20);
+  }
+
+  println!();
+
+  {
+    let my_number = HoldsANumber(20);
+    println!("{:?}", my_number.checked_sub(100)); // 20 - 100 = -80; doesn't fit in u8
+    my_number.prints_the_number_times_two();
+  }
+
+  println!();
+
+  // DerefMut
+  {
+    let mut my_number = HoldsANumber(20);
+    *my_number = 30; // DerefMut lets us do this
+    println!("{:?}", my_number.checked_sub(100));
+    my_number.prints_the_number_times_two();
+  }
+
+  println!();
+
+  {
+    let brandy1 = Character::new(
+      "Brandy1".to_string(),
+      9,
+      8,
+      7,
+      10,
+      19,
+      19,
+      5,
+      Alignment::Good,
+    );
+
+    let brandy2 = Character::new(
+      "Brandy2".to_string(),
+      9,
+      8,
+      7,
+      10,
+      19,
+      19,
+      5,
+      Alignment::Good,
+    );
+
+    let mut hit_points = vec![];
+    hit_points.push(*brandy1); // ⚠️ looks frigging WEIRD! 
+    hit_points.push(*brandy2); // ⚠️ looks frigging WEIRD! 
+
+    println!("{:?}", hit_points);
+  }
 }
