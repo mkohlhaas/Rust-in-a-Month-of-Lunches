385f8bac336c255ef5d097e2bae87f90a843b503
Hans-J. Schmid <knock@myopendoor.de>
2026-06-05 18:34:34 +0200

    cargo

--- easy-rust-179/src/main.rs
+++ easy-rust-179/src/main.rs
@@ -1,3 +1,18 @@
+// cargo
+// https://dhghomon.github.io/easy_rust/Chapter_62.html
+// https://www.youtube.com/watch?v=7LeIoU85XCI&list=PLfllocyHVgsRwLkTAhG0E-2QxCf-ozBkk&index=179&pp=iAQB
+
+use std::fmt::Display;
+
+fn print_and_return_thing<T: Display>(input: T) -> T {
+  println!("You gave me {} and now I will give it back.", input);
+  input
+}
+
 fn main() {
-  println!("Hello, world!");
+  let my_name = print_and_return_thing("Windy");
+  let small_number = print_and_return_thing(9.0);
+
+  println!("{:?}", my_name);
+  println!("{:?}", small_number);
 }
